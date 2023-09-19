e01d239dbdf2ef2059bbeadd521db9aa7de3af9e
jehryl <tjeffdarhyl@gmail.com>
2023-09-19 19:56:05 +0200

    fifth commit

--- python_app.py
+++ python_app.py
@@ -3,20 +3,30 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def hello_world():
     prefix_google = """
- <!-- Google tag (gtag.js) -->
-<script async
-src="https://www.googletagmanager.com/gtag/js?id=G-3G2B4QB8XY"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3G2B4QB8XY"></script>
 <script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', ' G-3G2B4QB8XY');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-3G2B4QB8XY');
 </script>
- """
-    return prefix_google + "Hello World"
+"""
+    button_html = """
+<button onclick="sendEventToGoogleAnalytics()">Send Event to Google Analytics</button>
+<script>
+  function sendEventToGoogleAnalytics() {
+    gtag('event', 'button_click', {
+      'event_category': 'Custom Events',
+      'event_label': 'Button Clicked'
+    });
+    alert('Event sent to Google Analytics!');
+  }
+</script>
+"""
+    return prefix_google + "Hello World" + button_html
 
 if __name__ == "__main__":
     app.run()
