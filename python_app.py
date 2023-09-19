4e4b3458af4a515f8d08278749fc6d94abc1ad82
jehryl <tjeffdarhyl@gmail.com>
2023-09-19 17:02:46 +0200

    third commit

--- python_app.py
+++ python_app.py
@@ -3,5 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+
 def hello_world():
-    return "Hello from Space! 🚀"
+    prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-3G2B4QB8XY"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' G-3G2B4QB8XY');
+</script>
+ """
+    return prefix_google + "Hello World"
+
+if __name__ == "__main__":
+    app.run()
