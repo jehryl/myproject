2d0d2bc66b06120286516cb5f008514ffbdd320b
jehryl <tjeffdarhyl@gmail.com>
2023-10-03 21:42:19 +0200

    new commit with logger

--- python_app.py
+++ python_app.py
@@ -1,32 +1,44 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    prefix_google = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-3G2B4QB8XY"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-3G2B4QB8XY');
-</script>
-"""
+    prefix_google = """ <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3G2B4QB8XY"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-3G2B4QB8XY');
+    </script>
+    """
     button_html = """
-<button onclick="sendEventToGoogleAnalytics()">Send Event to Google Analytics</button>
-<script>
-  function sendEventToGoogleAnalytics() {
-    gtag('event', 'button_click', {
-      'event_category': 'Custom Events',
-      'event_label': 'Button Clicked'
-    });
-    alert('Event sent to Google Analytics!');
-  }
-</script>
-"""
+    <button onclick="sendEventToGoogleAnalytics()">Send Event to Google Analytics</button>
+    <script>
+      function sendEventToGoogleAnalytics() {
+        gtag('event', 'button_click', {
+          'event_category': 'Custom Events',
+          'event_label': 'Button Clicked'
+        });
+        alert('Event sent to Google Analytics!');
+      }
+    </script>
+    """
     return prefix_google + "Hello World" + button_html
+
+@app.route("/logger")
+def logger():
+    # Log on the server side (Python)
+    app.logger.info("This is a server-side log.")
+
+    # Log on the browser side
+    log_script = """
+    <script>
+      console.log("This is a browser-side log.");
+    </script>
+    """
+    return "Logger Page" + log_script
 
 if __name__ == "__main__":
     app.run()
